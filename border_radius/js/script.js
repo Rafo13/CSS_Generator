78c8919ac3aa36de3c32521f40997fdba9c35409
Rafo13 <rafoarakelyan33gmail.com>
2020-09-09 01:35:37 +0400

    katarecinq commit verjin popoxutynnery pahpanelu hamar

--- border_radius/js/script.js
+++ border_radius/js/script.js
@@ -14,14 +14,49 @@ let borderRang = document.querySelector('.border-rang')
 
 let borderPosition = document.querySelectorAll('.border-position-select')
 let borderPosition1 = document.querySelector('.border-position-select')
+
 let borderStyle = document.querySelectorAll('.border-style-select')
 let borderStyle1 = document.querySelector('.border-style-select')
-// console.log(borderPosition)
+
+let bordColor = document.querySelector('.bord-color')
+let backColor = document.querySelector('.back-color')
+
+let result = document.querySelector('.result')
+let btn = document.querySelector('.result > .button')
+
+let pBorderStyle = document.querySelector('.p-border-style')
+let pBorderWidth = document.querySelector('.p-border-width')
+let pBorderColor = document.querySelector('.p-border-color')
+let pBorderRadius = document.querySelector('.p-border-radius')
+let pBackgroundColor = document.querySelector('.p-background-color')
+
+
+bordColor.addEventListener('input', funcBordColor)
+backColor.addEventListener('input', funcBackColor)
+
+
+//փոխում է border-ի գույնը
+funcBordColor()
+function funcBordColor() {
+   div.style.borderColor = bordColor.value;
+   pBorderColor.innerHTML = bordColor.value + ';'
+}
+//փոխում է background-ի գույնը
+backColor.value = '#247a64'
+funcBackColor()
+function funcBackColor() {
+   div.style.backgroundColor = backColor.value;
+   pBackgroundColor.innerHTML = backColor.value + ';'
+}
+
+//border-ի կողմերը փոխվելու ժամանակ կանչվում է նշված ֆունկցիան
 for (let i = 0; i < borderPosition.length; i++) {
    borderPosition[i].onchange = function () {
       funcBorder()
    }
 }
+
+//border-ի ոճը փոխվելու ժամանակ կանչվում է նշված ֆունկցիան
 for (let i = 0; i < borderStyle.length; i++) {
    borderStyle[i].onchange = function () {
       funcBorder()
@@ -35,33 +70,64 @@ funcBorder()
 
 function funcBorder() {
    borderInp.value = borderRang.value
-   console.log(borderPosition1.value)
    if (borderPosition1.value == 'All') {
       div.style.borderWidth = borderRang.value + 'px';
-   }
-   if (borderPosition1.value == 'Left') {
+      pBorderWidth.innerHTML = borderRang.value + 'px ' + borderRang.value + 'px ' + borderRang.value + 'px ' + borderRang.value + 'px' + ';'
+   } else if (borderPosition1.value == 'Left') {
       div.style.borderLeftWidth = borderRang.value + 'px';
       div.style.borderRightWidth = 0 + 'px';
       div.style.borderBottomWidth = 0 + 'px';
       div.style.borderTopWidth = 0 + 'px';
-   }
-   if (borderPosition1.value == 'Right') {
+      pBorderWidth.innerHTML = 0 + 'px ' + 0 + 'px ' + 0 + 'px ' +  borderRang.value + 'px' + ';'
+   } else if (borderPosition1.value == 'Right') {
       div.style.borderLeftWidth = 0 + 'px';
       div.style.borderRightWidth = borderRang.value + 'px';
       div.style.borderBottomWidth = 0 + 'px';
       div.style.borderTopWidth = 0 + 'px';
-   }
-   if (borderPosition1.value == 'Bottom') {
+      pBorderWidth.innerHTML = 0 + 'px ' + borderRang.value + 'px ' + 0 + 'px ' + 0 + 'px' + ';'
+   } else if (borderPosition1.value == 'Bottom') {
       div.style.borderLeftWidth = 0 + 'px';
       div.style.borderRightWidth = 0 + 'px';
       div.style.borderBottomWidth = borderRang.value + 'px';
       div.style.borderTopWidth = 0 + 'px';
-   }
-   if (borderPosition1.value == 'Top') {
+      pBorderWidth.innerHTML = 0 + 'px ' + 0 + 'px ' + borderRang.value + 'px ' + 0 + 'px' + ';'
+   } else if (borderPosition1.value == 'Top') {
       div.style.borderLeftWidth = 0 + 'px';
       div.style.borderRightWidth = 0 + 'px';
       div.style.borderBottomWidth = 0 + 'px';
       div.style.borderTopWidth = borderRang.value + 'px';
+      pBorderWidth.innerHTML = borderRang.value  + 'px ' + 0 + 'px ' + 0 + 'px ' + 0 + 'px' + ';'
+   }
+
+  
+
+   if (borderStyle1.value == 'solid') {
+      pBorderStyle.innerHTML = 'solid' + ';'
+      div.style.borderStyle = 'solid'
+   } else if (borderStyle1.value == 'dashed') {
+      pBorderStyle.innerHTML = 'dashed' + ';'
+      div.style.borderStyle = 'dashed'
+   } else if (borderStyle1.value == 'dotted') {
+      pBorderStyle.innerHTML = 'dotted' + ';'
+      div.style.borderStyle = 'dotted'
+   } else if (borderStyle1.value == 'inset') {
+      pBorderStyle.innerHTML = 'inset' + ';'
+      div.style.borderStyle = 'inset'
+   } else if (borderStyle1.value == 'outset') {
+      pBorderStyle.innerHTML = 'outset' + ';'
+      div.style.borderStyle = 'outset'
+   } else if (borderStyle1.value == 'groove') {
+      pBorderStyle.innerHTML = 'groove' + ';'
+      div.style.borderStyle = 'groove'
+   } else if (borderStyle1.value == 'ridge') {
+      pBorderStyle.innerHTML = 'ridge' + ';'
+      div.style.borderStyle = 'ridge'
+   } else if (borderStyle1.value == 'hidden') {
+      pBorderStyle.innerHTML = 'hidden' + ';'
+      div.style.borderStyle = 'hidden'
+   } else if (borderStyle1.value == 'none') {
+      pBorderStyle.innerHTML = 'none' + ';'
+      div.style.borderStyle = 'none'
    }
 
 }
@@ -80,6 +146,7 @@ function func1() {
    genInp.value = genRang.value
 
    div.style.borderRadius = 2 * +inpRang[0].value + 'px' + ' ' + 2 * +inpRang[1].value + 'px' + ' ' + 2 * +inpRang[3].value + 'px' + ' ' + 2 * +inpRang[2].value + 'px';
+   pBorderRadius.innerHTML = 2 * +inpRang[0].value + 'px' + ' ' + 2 * +inpRang[1].value + 'px' + ' ' + 2 * +inpRang[3].value + 'px' + ' ' + 2 * +inpRang[2].value + 'px' + ';'
 }
 func1()
 
@@ -93,5 +160,13 @@ function func() {
       inp[i].value = inpRang[i].value
    }
    div.style.borderRadius = 2 * +inpRang[0].value + 'px' + ' ' + 2 * +inpRang[1].value + 'px' + ' ' + 2 * +inpRang[3].value + 'px' + ' ' + 2 * +inpRang[2].value + 'px';
+   pBorderRadius.innerHTML = 2 * +inpRang[0].value + 'px' + ' ' + 2 * +inpRang[1].value + 'px' + ' ' + 2 * +inpRang[3].value + 'px' + ' ' + 2 * +inpRang[2].value + 'px' + ';'
 }
 func()
+
+
+btn.addEventListener('click', funcButton)
+function funcButton(e){
+   e.preventDefault()
+   console.log(10)
+}
